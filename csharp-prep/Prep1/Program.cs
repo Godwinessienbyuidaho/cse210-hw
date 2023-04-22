5801d00217edaaf325874c51dd926467c4bd7f34
Godwinesssienbyuidaho <liahona911@gmail.com>
2023-04-22 15:14:34 +0100

    c# prep 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine();   
+        Console.Write("what is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+        String lastname = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage grade? ");
+        String studentGrade = Console.ReadLine();
+        int percent = int.Parse(studentGrade);
+
+        string letterGrade = "";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you didnt do very well this time!");
+        }
     }
 }
